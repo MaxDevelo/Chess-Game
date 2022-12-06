a39b6aa93d490d9f22c08e02628c11f31e3990bb
MaxDevelo <max.developperfb@gmail.com>
2022-12-06 07:01:20 +0100

    Implémentation du mouvement du roi

--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -244,6 +244,10 @@ public class BoardView extends JFrame{
                     square = _board.getSquare(this._currentButtonPiece);
                     _controller.validMove(square);
                     btnMove = true; // A ENLEVER
+                }else if(btnPiece.getName().equals("K")) {
+                    square = _board.getSquare(this._currentButtonPiece);
+                    _controller.validMove(square);
+                    btnMove = true; // A ENLEVER
                 }
                 if(btnMove){ // A ENLEVER
                     for(Square s : _board.getValidSquares()){
