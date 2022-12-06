c6b8830f56cf1a1f9ecb244317bb6a8076945b2e
MaxDevelo <max.developperfb@gmail.com>
2022-12-06 06:54:56 +0100

    Implémentation du mouvement de la Reine

--- src/Model/Pieces/Queen.java
+++ src/Model/Pieces/Queen.java
@@ -16,7 +16,20 @@ public class Queen  extends Piece {
 
     @Override
     public Boolean canMove(Square square, Square currentSquare) {
-        return null;
+        if(square.getPiece() == null || square.getPiece().getColor() != currentSquare.getPiece().getColor()){
+             if(up(square, currentSquare)){
+                return true;
+            }else if(down(square, currentSquare)){
+                return true;
+            }else if(left(square, currentSquare)){
+                return true;
+            }else if(right(square, currentSquare)){
+                 return true;
+             }else{
+                return false;
+            }
+        }
+        return false;
     }
 
 
@@ -30,21 +43,41 @@ public class Queen  extends Piece {
     }
     @Override
     public Boolean up(Square square, Square currentSquare) {
+        if(square.getRow() == currentSquare.getRow()-1 && square.getColumn() == currentSquare.getColumn()){
+            return true;
+        }
         return false;
     }
 
     @Override
     public Boolean down(Square square, Square currentSquare) {
+        if(square.getRow() == currentSquare.getRow()+1 && square.getColumn() == currentSquare.getColumn()){ // DOWN
+            return true;
+        }
         return false;
     }
 
     @Override
     public Boolean left(Square square, Square currentSquare) {
+        if(square.getRow() == currentSquare.getRow() && square.getColumn() == currentSquare.getColumn()-1){ // LEFT
+            return true;
+        }else if(square.getRow() == currentSquare.getRow()+1 && square.getColumn() == currentSquare.getColumn()-1){
+            return true;
+        }else if(square.getRow() == currentSquare.getRow()-1 && square.getColumn() == currentSquare.getColumn()-1){
+            return true;
+        }
         return false;
     }
 
     @Override
     public Boolean right(Square square, Square currentSquare) {
+        if(square.getRow() == currentSquare.getRow() && square.getColumn() == currentSquare.getColumn()+1){ // RIGHT
+            return true;
+        }else if(square.getRow() == currentSquare.getRow()-1 && square.getColumn() == currentSquare.getColumn()+1){
+            return true;
+        }else if(square.getRow() == currentSquare.getRow()+1 && square.getColumn() == currentSquare.getColumn()+1){
+            return true;
+        }
         return false;
     }
 }
--- src/View/BoardView.java
+++ src/View/BoardView.java
@@ -240,6 +240,10 @@ public class BoardView extends JFrame{
                     square = _board.getSquare(this._currentButtonPiece);
                     _controller.validMove(square);
                     btnMove = true; // A ENLEVER
+                }else if(btnPiece.getName().equals("Q")) {
+                    square = _board.getSquare(this._currentButtonPiece);
+                    _controller.validMove(square);
+                    btnMove = true; // A ENLEVER
                 }
                 if(btnMove){ // A ENLEVER
                     for(Square s : _board.getValidSquares()){
